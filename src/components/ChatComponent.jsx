eefd6a59db1995d1daa721f3b435d27c949e7265
Linesh Malkam <103716615+lmalkam@users.noreply.github.com>
2024-02-01 20:41:50 +0530

    loading button

--- src/components/ChatComponent.jsx
+++ src/components/ChatComponent.jsx
@@ -21,6 +21,7 @@ export default function ChatComponent() {
 
   const [userInput, setUserInput] = useState('');
   const [pdfData, setpdfData] = useState('');
+  const [waiting, setwait] = useState(false);
 
 
   useEffect(() => {
@@ -64,7 +65,8 @@ export default function ChatComponent() {
       const data = await response.json();
 
       console.log('Server response:', data.message);
-      return data.message
+      
+      return data.message;
     } catch (error) {
       console.error('Error sending data to server:', error.message);
     }
@@ -79,10 +81,11 @@ export default function ChatComponent() {
       // Add the user's message to the messages state
       setMessages([...messages, { role: 'user', content: userInput.trim() }]);
       setUserInput('');
-
+      setwait(true);
       // Assuming sendDataToServer returns the new message
       const newMessage = await sendDataToServer();
 
+      setwait(false);
       // Append the new message to the existing messages
       setMessages(prevMessages => [...prevMessages, { role: 'assistant', content: newMessage }]);
     }
@@ -110,7 +113,7 @@ export default function ChatComponent() {
         {/* Input field for user message */}
         <input
           type='text'
-          placeholder='Message chatbot'
+          placeholder= { waiting ? 'Please Wait' :' Message chatbot '}
           className='input input-bordered w-full'
           value={userInput}
           onChange={(e) => setUserInput(e.target.value)}
@@ -118,9 +121,12 @@ export default function ChatComponent() {
         />
         
         {/* Button to send message */}
-        <button className='btn btn-square btn-secondary' onClick={sendMessage}>
+        <span>
+        { waiting ? <span className="loading loading-spinner loading-lg"></span> : <button className='btn btn-square btn-secondary' onClick={sendMessage}>
           <SendIcon />
-        </button>
+        </button>}
+        </span>
+        
         
       </div>
     </div>
