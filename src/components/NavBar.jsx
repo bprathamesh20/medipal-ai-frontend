1128282256fafa5899cf4899e870bf34a013cbd9
Ninaad783 <ninaadkumbhar@gmail.com>
2024-02-01 23:02:58 +0530

    added avtar to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,20 @@
 import { SignOutButton } from '@clerk/clerk-react'
 import React from 'react'
+import avatarImg from '../assets/chatbotImg.svg'
 
 
 export default function NavBar() {
   return (
     <div className="navbar bg-base-100 border-b border-secondary">
   <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-      </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Homepage</a></li>
-        <li><a>Portfolio</a></li>
-        <li><a>About</a></li>
-      </ul>
-    </div>
+
+  <div className="avatar">
+  <div className="w-8 rounded-full">
+    <img src={avatarImg} alt="Tailwind-CSS-Avatar-component" />
+  </div>
+</div>
+      
+    
   </div>
   <div className="navbar-center">
     <a className="btn btn-ghost text-xl" onClick={() => navigate('/')}>medipal.ai</a>
